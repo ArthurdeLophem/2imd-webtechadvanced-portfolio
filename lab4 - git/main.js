72cd4c78cc42f2ce268c7fbb8230c6dd1364dc68
r0808 <arthurdelophem3@gmail.com>
2022-03-12 21:03:09 +0100

    feat: temporarily remove api calls for style purpose

--- lab4 - git/main.js
+++ lab4 - git/main.js
@@ -1,4 +1,4 @@
 import './style.css'
-import App from './classes/App.js'
+/*import App from './classes/App.js'
 
-const app = new App("8668829ac4b2bf74df5acdc9b0c20de0");
+const app = new App("8668829ac4b2bf74df5acdc9b0c20de0");*/
